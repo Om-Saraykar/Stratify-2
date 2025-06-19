06a115f364fd663e37d741a0c6d77ba886ab7f26
Om-Saraykar <omsaraykar@gmail.com>
2025-06-19 14:08:39 +0530

    added login and signup page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button'
+import { Navbar1 } from '@/components/landing/navbar';
+import { Hero12 } from '@/components/landing/hero';
+import { Blog7 } from '@/components/landing/blog';
+import { Footer7 } from '@/components/landing/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-      <h1>Hello There, Welcome to Stratify 2!</h1>
-      <Link href="/dashboard">
-        <Button className='cursor-pointer'>
-          Go to Dashboard
-        </Button>
-      </Link>
+    <div>
+      <Navbar1 />
+      <div className="flex flex-col items-center justify-center py-2 gap-4">
+        <Hero12 />
+        <Blog7 />
+        <Footer7 />
+      </div>
     </div>
   );
 }
